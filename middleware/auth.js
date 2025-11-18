85b8ae89bbe3cc6b76922e48aa6aaa92a4ba0010
codemaster1022 <codemaster.han1022@gmail.com>
2025-11-17 19:01:24 -0800

    updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -71,6 +71,9 @@ const checkSpeaker = (req, res, next) => {
   next();
 };
 
+
+
+
 const authorizeRoles = (...roles) => (req, res, next) => {
   if (!req.user || !roles.includes(req.user.role)) {
     return res.status(403).json({
